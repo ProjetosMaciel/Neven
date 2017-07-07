2ccced0a96d68a36054b794d19d619225ec3f77d
OtavioDePizzol <depizzol94@gmail.com>
2017-07-06 23:35:11 -0300

    Merge branch 'master' of https://github.com/ProjetosMaciel/Neven

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,7 @@
 angular.module('starter.controllers', ['firebase','ngOpenFB'])
 //teste********** como vou fazer este teste mais um
 
-// Otávio De Pizzol fez uma alteração aqui
+// Otávio De Pizzol
 
 .controller('LivrariasCtrl', function($scope, IonicLogin, $ionicPopup, $http, $ionicLoading, $state, $rootScope) {
 
