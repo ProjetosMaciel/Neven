b6f2c063cf8565f05ab9c59af233b538d54caae1
ProjetosMaciel <EvertonMaciel93@hotmail.com>
2017-07-06 21:04:54 -0300

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,5 @@
 angular.module('starter.controllers', ['firebase','ngOpenFB'])
-
+//teste**********
 .controller('LivrariasCtrl', function($scope, IonicLogin, $ionicPopup, $http, $ionicLoading, $state, $rootScope) {
 
 	$scope.$on('$ionicView.enter', function(e) {
